34f57f064d93f498990e804d651cd51175b934c2
Steffe Pans <71630878+Steffe-Pans@users.noreply.github.com>
2023-02-05 17:35:30 +0100

    Sectie_6 Source

---
+++ Sectie_6/Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,20 @@
+// Steffe Pans - 3APP01
+
+
+#include "BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue() 
+{
+    NodeName = "Clear Blackboard Value";
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory) 
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+
+    OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+
+    return EBTNodeResult::Succeeded;
+}
---
+++ Sectie_6/Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,39 @@
+// Steffe Pans - 3APP01
+
+
+#include "ShooterAIController.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "ShooterCharacter.h"
+
+
+void AShooterAIController::BeginPlay() 
+{
+    Super::BeginPlay();
+
+    if (AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+
+        APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+    }
+}
+
+void AShooterAIController::Tick(float DeltaSeconds) 
+{
+    Super::Tick(DeltaSeconds);
+}
+
+bool AShooterAIController::IsDead() const
+{
+    AShooterCharacter* ControlledCharacter = Cast<AShooterCharacter>(GetPawn());
+    if (ControlledCharacter != nullptr)
+    {
+        return ControlledCharacter->IsDead();
+    }
+
+    return true;
+}
